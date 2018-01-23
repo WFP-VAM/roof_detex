c3d927426cde2533c8edd9199c0cdfd0a6798947
Lorenzo <lo.riches@gmail.com>
2018-01-23 13:03:37 +0000

    not learning with augmentation?

--- player.py
+++ player.py
@@ -8,7 +8,7 @@ from tensorflow.python.keras.callbacks import TensorBoard
 # PARAMETERS ------------
 img_rows, img_cols = 400, 400
 classes = 1
-batch_size = 16
+batch_size = 4
 epochs = 20
 # because of MaxPool layer width and height has to be divisible by 2^4
 # use 320 on the rows to avoid the Google sign.
@@ -33,39 +33,28 @@ for file in os.listdir('masks/1/masks'):
 training_images = np.array(training_images)
 training_masks = np.array(training_masks)[:, :, :, 0].reshape(len(training_masks), img_rows, img_cols, 1)
 
+# normalize RGB images
+training_images = training_images.astype('float32')
+mean = np.mean(training_images)  # mean for data centering
+std = np.std(training_images)  # std for data normalization
+training_images -= mean
+training_images /= std
+b=training_images[:,:,:,0]
+g=training_images[:,:,:,1]
+r=training_images[:,:,:,2]
+sum=b+g+r
+training_images[:,:,:,0]=b/sum*255.0
+training_images[:,:,:,1]=g/sum*255.0
+training_images[:,:,:,2]=r/sum*255.0
+
+
+
 # generators for data augmentation -------
 seed = 1
 generator_x = ImageDataGenerator(
-    featurewise_center=True,
-    featurewise_std_normalization=True,
-    vertical_flip=True,
-    horizontal_flip=True)
-
-generator_y = ImageDataGenerator(
-    featurewise_center=False,
-    featurewise_std_normalization=False,
-    vertical_flip=True,
-    horizontal_flip=True)
+    vertical_flip=False)
 
 generator_x.fit(training_images, augment=True, seed=seed)
-generator_y.fit(training_masks, augment=True, seed=seed)
-
-
-image_generator = generator_x.flow_from_directory(
-    'GiveDirectlyData/data',
-    target_size=(img_rows, img_cols),
-    class_mode=None,
-    seed=seed)
-
-mask_generator = generator_y.flow_from_directory(
-    'masks/1',
-    target_size=(img_rows, img_cols),
-    class_mode=None,
-    color_mode='grayscale',
-    seed=seed)
-
-# combine generators into one which yields image and masks
-train_generator = zip(image_generator, mask_generator)
 
 # load model
 model = unet(img_rows, img_cols, classes=classes)
@@ -73,7 +62,8 @@ model = unet(img_rows, img_cols, classes=classes)
 # kick off training
 tb = TensorBoard(log_dir='logs', histogram_freq=False,  write_graph=False, write_images=False)
 
-model.fit_generator(train_generator, steps_per_epoch=int(len(training_images)/batch_size),
+model.fit_generator(generator_x.flow(training_images, training_masks, batch_size=batch_size), 
+                    steps_per_epoch=int(len(training_images)/batch_size),
                     epochs=epochs, callbacks=[tb])
 
 # save model
