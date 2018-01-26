553c8aefb20c4b0596701e249e4b870af071223b
lorenzo.riches <lorenzo.riches@wfp.org>
2018-01-26 11:13:06 +0100

    rotate images and masks by 90

--- scripts/augmentation.py
+++ scripts/augmentation.py
@@ -1,17 +1,28 @@
 import os
 from PIL import Image
 
-def flip_90_in_dir(directory):
+bad_images = ['KE2013071222-grass.png', 'KE2013071223-grass.png', 'KE2013071224-grass.png', 'KE2013071225-grass.png',
+              'KE2013071037-iron.png', 'KE2013071038-iron.png', 'KE2013071039-iron.png', 'KE2013071040-iron.png',
+              'KE2013071041-iron.png', 'KE2013071121-iron.png', 'KE2013071223-grass.png', 'KE2013071204-grass.png'
+              'KE2013071224-grass.png', 'KE2013071225-grass.png', 'KE2013071205-grass.png', 'KE2013071206-grass.png',
+              'KE2013071940-iron.png', 'KE2013071941-iron.png', 'KE2013071942-iron.png', 'KE2013071943-iron.png',
+              'KE2013072373-grass.png']
+
+
+def flip_pm90_in_dir(directory):
+    """ rotates all the images in a directory by + and - 90 degrees"""
     for file in os.listdir(directory):
-        if file.endswith(".png"):
+        if file.endswith(".png") & (str(file) not in bad_images):
+            print(str(file))
             im = Image.open(directory + file, 'r')
 
-            im = im.rotate(90)
+            imp = im.rotate(90)
+            imp.save(directory + file[:-4] + '_p90.png')
 
-            im.save(directory + file[4:] + '_90.png')
+            imm = im.rotate(-90)
+            imm.save(directory + file[:-4] + '_m90.png')
 
 
-
-flip_90_in_dir('GiveDirectlyData/data/images/')
-flip_90_in_dir('masks/2/')
+flip_pm90_in_dir('GiveDirectlyData/data/images/')
+flip_pm90_in_dir('masks/1/masks/')
 
